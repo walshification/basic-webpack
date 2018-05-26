bd0489f2e5c582b5abf9ccc96d4f54a8aca336ba
Christopher Walsh <walshification@gmail.com>
2018-05-26 11:47:47 -0500

    creates a new module for inserting an img into the app

--- build/bundle.js
+++ build/bundle.js
@@ -63,11 +63,23 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var image = document.createElement('img');
+image.src = 'http://lorempixel.com/400/400';
+
+document.body.appendChild(image);
+
+/***/ },
+/* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -83,17 +95,21 @@ var sum = function sum(a, b) {
 exports.default = sum;
 
 /***/ },
-/* 1 */
+/* 2 */
 /***/ function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-var _sum = __webpack_require__(0);
+var _sum = __webpack_require__(1);
 
 var _sum2 = _interopRequireDefault(_sum);
 
+__webpack_require__(0);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// imported just so it will run
 
 var total = (0, _sum2.default)(10, 5);
 console.log(total);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import sum from './sum';
+import './image_viewer'; // imported just so it will run
 
 const total = sum(10, 5);
 console.log(total);
